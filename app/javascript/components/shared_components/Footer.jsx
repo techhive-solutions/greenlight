a52a92bece96d0ddba6afb30fb3e0ce53e1d0917
muhammadk1607 <muhammadkhan1607@gmail.com>
2026-03-09 19:41:43 +0500

    🐛 Fix footer

--- app/javascript/components/shared_components/Footer.jsx
+++ app/javascript/components/shared_components/Footer.jsx
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License along
 // with Greenlight; if not, see <http://www.gnu.org/licenses/>.
 
+import React from "react";
 import { Container } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import { useAuth } from "../../contexts/auth/AuthProvider";
